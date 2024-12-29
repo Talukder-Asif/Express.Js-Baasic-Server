807543aecde277bd6bea180716c99ccdc416d4ab
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2024-12-29 09:39:06 +0600

    Middleware

--- dist/app.js
+++ dist/app.js
@@ -8,10 +8,15 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get('/', (req, res) => {
+// Middleware
+const logger = (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+};
+app.get('/', logger, (req, res) => {
     res.send('Hello World!');
 });
-app.post('/', (req, res) => {
+app.post('/', logger, (req, res) => {
     console.log(req.body);
     res.send('POST request received!');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 const app = express();
 
 // parsers
 app.use(express.json());
 app.use(express.text());
 
-app.get('/', (req : Request, res: Response)=>{
+// Middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.method, req.url);
+    next();
+}
+
+app.get('/',logger, (req : Request, res: Response)=>{
     res.send('Hello World!');
 })
 
-app.post('/', (req: Request, res: Response)=>{
+app.post('/',logger, (req: Request, res: Response)=>{
     console.log(req.body);
     res.send('POST request received!');
 })
