1d3a87d5e2773559ee039b639b17672ceb026121
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2024-12-29 11:57:45 +0600

    Basic Router in express js explor

--- dist/app.js
+++ dist/app.js
@@ -13,6 +13,31 @@ const logger = (req, res, next) => {
     console.log(req.method, req.url);
     next();
 };
+// Router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/api/v1/users', userRouter);
+userRouter.post('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        status: 201, // HTTP status code for success
+        success: true, // true if the request was successful
+        data: user, // the created user
+        message: 'User created successfully!',
+    });
+});
+app.use('/api/v1/courses', courseRouter);
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        status: 201, // HTTP status code for success
+        success: true, // true if the request was successful
+        data: course, // the created course
+        message: 'Course created successfully!',
+    });
+});
 app.get('/', logger, (req, res) => {
     res.send('Hello World!');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,51 @@
 import express, { NextFunction, Request, Response } from 'express';
 const app = express();
 
+
 // parsers
 app.use(express.json());
 app.use(express.text());
+
 
 // Middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.method, req.url);
     next();
 }
+
+
+
+// Router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use('/api/v1/users', userRouter);
+userRouter.post('/create-user', (req: Request, res: Response)=>{
+    const user = req.body;
+    console.log(user);
+    res.json({ 
+        status: 201,  // HTTP status code for success
+        success: true,  // true if the request was successful
+        data: user,  // the created user
+        message: 'User created successfully!',
+    });
+})
+
+
+app.use('/api/v1/courses', courseRouter);
+
+courseRouter.post('/create-course', (req: Request, res: Response)=>{
+    const course = req.body;
+    console.log(course);
+    res.json({
+        status: 201,  // HTTP status code for success
+        success: true,  // true if the request was successful
+        data: course,  // the created course
+        message: 'Course created successfully!',
+    })
+})
+
+
 
 app.get('/',logger, (req : Request, res: Response)=>{
     res.send('Hello World!');
